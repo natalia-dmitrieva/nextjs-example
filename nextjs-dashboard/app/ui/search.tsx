2bf0ffc723ceddeae56c4c78b703ead6c0b2d5f7
Natalia Dmitrieva <idmitrievanm@gmail.com>
2024-12-09 15:34:26 +1300

    + temporary fix for next js issue with canary release - https://github.com/vercel/next.js/issues/46360

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
+// @ts-ignore
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
 	const searchParams = useSearchParams()
