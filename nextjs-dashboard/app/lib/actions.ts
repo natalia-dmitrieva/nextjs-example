f2124ade7b2998668ddac3a1300d2444767040db
Natalia Dmitrieva <idmitrievanm@gmail.com>
2024-12-09 21:09:19 +1300

    + handled revalidate and redirect after db updates

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -2,6 +2,9 @@
 
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+// @ts-ignore
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
 	id: z.string(),
@@ -26,4 +29,7 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+
+	revalidatePath('/dashboard/invoices');
+	redirect('/dashboard/invoices');
 }
